e2978383924b0ad8878f7e17232202658d099fce
Connor House <connorhouse19@gmail.com>
2019-10-10 22:18:03 -0400

    Create Lab7.py

---
+++ Lab7.py
@@ -0,0 +1,15 @@
+# Exercise 8.9 (in the Chapter) - Now that you have seen the details of the railDecrypt function you can make it smarter in two ways:
+    # You do not need to check cases where the number of rails is greater than the message length divided by two. Can you explain why?
+    # You only need to check cases where the number of rails evenly divides the total message length. Can you explain why?
+
+
+
+# Exercise 8.14 (in the Chapter) - Write a function wordPop that accepts a text and a length N and returns the list of all the words that are N letters long, sorted by their length.
+
+
+
+# Exercise 8.18 - Write a regular expression pattern to match all words ending in ing.
+
+
+
+# Exercise 8.20 - Write a regular expression pattern to match all words beginning and ending with the letter a.
